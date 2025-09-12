8e4add0f1285db08635322d26450de00c6e41e6e
Darsh <darshdcoding@gmail.com>
2025-09-13 00:27:06 +0530

    completed lession 6 exercises from 6a to 6d

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -1,36 +1,12 @@
+import CheckoutHeader from "./CheckOutHeader";
 import "./CheckoutPage.css";
-import "./checkout-header.css";
 
 const CheckoutPage = () => {
   return (
     <>
       <title>Checkout</title>
 
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <a href="/">
-              <img className="logo" src="./src/assets/images/logo.png" />
-              <img
-                className="mobile-logo"
-                src="./src/assets/images/mobile-logo.png"
-              />
-            </a>
-          </div>
-
-          <div className="checkout-header-middle-section">
-            Checkout (
-            <a className="return-to-home-link" href="/">
-              3 items
-            </a>
-            )
-          </div>
-
-          <div className="checkout-header-right-section">
-            <img src="./src/assets/images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
+      <CheckoutHeader/>
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
