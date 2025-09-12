21770fab0d2e98a4ccde4fff650e806e51426425
Darsh <darshdcoding@gmail.com>
2025-09-12 15:24:17 +0530

    Created and added react routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import HomePage from './pages/HomePage'
-// import { useState } from 'react'
-import './App.css'
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <HomePage/>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route 
+          path="checkout" 
+          element={<div>Test Checkout Page...</div>} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
