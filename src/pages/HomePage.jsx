fc765162d984bac15776592fe27a82833eb104af
Darsh D. Coding <darshdcoding@gmail.com>
2025-09-14 17:31:48 +0530

    fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,12 @@ import { products } from "../../starting-code/ecommerce-project/data/products";
 import "./HomePage.css";
 
 const HomePage = () => {
+  fetch('http://localhost:3000/api/products/')
+  .then((response)=>{
+  return response.json();
+}).then((data)=>{
+  console.log(data)
+  })
   return (
     <>
       <title>E-commerce Project</title>
