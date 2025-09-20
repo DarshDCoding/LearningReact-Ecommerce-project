e121e070970bea890f0836b484257af2035e379e
Darsh D. Coding <darshdcoding@gmail.com>
2025-09-21 00:32:15 +0530

    creating delete cart item feature

--- src/pages/checkout/OrderSummary.jsx
+++ src/pages/checkout/OrderSummary.jsx
@@ -1,17 +1,24 @@
 import dayjs from "dayjs";
 import { DeliveryOptions } from "./DeliveryOptions";
 import { CartItemDetails } from "../../components/CartItemDetails";
+import axios from "axios";
 
 export function OrderSummary({ cart, deliveryOptions, loadCart }) {
   return (
     <div className="order-summary">
       {deliveryOptions.length > 0 &&
         cart.map((cartItem) => {
+          
           const selectedDeliveryOption = deliveryOptions.find(
             (deliveryOption) => {
               return deliveryOption.id === cartItem.deliveryOptionId;
-            }
-          );
+            });
+
+          const deleteCartItem = async ()=>{
+            await axios.delete(`/api/cart-items/${cartItem.productId}`);
+            await loadCart();
+          }
+
           return (
             <div key={cartItem.productId} className="cart-item-container">
               <div className="delivery-date">
@@ -19,7 +26,7 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
               </div>
 
               <div className="cart-item-details-grid">
-                <CartItemDetails cartItem={cartItem} />
+                <CartItemDetails cartItem={cartItem} deleteCartItem={deleteCartItem}/>
                 <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
               </div>
             </div>
