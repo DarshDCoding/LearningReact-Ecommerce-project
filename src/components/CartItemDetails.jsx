85e60edb72db2b4e0026082c4cc2e457090c226b
Darsh D. Coding <darshdcoding@gmail.com>
2025-09-22 00:23:04 +0530

    adding a edge case, where if users enters 0 in the input field and update, the item will get deleted.

--- src/components/CartItemDetails.jsx
+++ src/components/CartItemDetails.jsx
@@ -1,27 +1,39 @@
 import axios from "axios";
 import { useState } from "react";
 import { formatMoney } from "../utils/money";
-import "./CartItemDetails.css"
+import "./CartItemDetails.css";
 
-export function CartItemDetails({ cartItem, deleteCartItem, loadCart}) {
-  const [isUpdate, setIsUpdate]= useState(false);
-  const [quantity, setQuantity]= useState(cartItem.quantity);
+export function CartItemDetails({ cartItem, deleteCartItem, loadCart }) {
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
 
-  const openUpdate = async()=>{
-    if (isUpdate){
-      await axios.put(`/api/cart-items/${cartItem.productId}`,{
-        productId:cartItem.productId,
-        quantity:Number(quantity)
-      })
-      loadCart();
-      setIsUpdate(false)
-    } else{
-      setIsUpdate(true)
+  const openUpdate = async () => {
+    if (isUpdate) {
+      setIsUpdate(false);
+
+      if (quantity == 0) {
+        deleteCartItem();
+        return 0;
+      }
+
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        productId: cartItem.productId,
+        quantity: Number(quantity),
+      });
+    } else {
+      setIsUpdate(true);
     }
+    loadCart();
   };
 
-  const getQuantity =(event)=>{
-    setQuantity (event.target.value);
+  // const handelEnter = (event) =>{
+  //   if (event.target.key === "Enter"){
+  //     openUpdate();
+  //   }
+  // };
+
+  const getQuantity = (event) => {
+    setQuantity(event.target.value);
   };
 
   return (
@@ -36,11 +48,36 @@ export function CartItemDetails({ cartItem, deleteCartItem, loadCart}) {
         <div className="product-quantity">
           <span>
             Quantity:
-            {isUpdate && <input type="number" style={{width:"50px", appearance:"textfield", WebkitAppearance:"textfield", MozAppearance:"textfield"}} value={quantity} onChange={getQuantity} />}
-            {!isUpdate && <span className="quantity-label">{cartItem.quantity}</span>}
+            {isUpdate && (
+              <input
+                type="number"
+                style={{
+                  width: "50px",
+                  appearance: "textfield",
+                  WebkitAppearance: "textfield",
+                  MozAppearance: "textfield",
+                }}
+                value={quantity}
+                onChange={getQuantity}
+                autoFocus
+              />
+            )}
+            {!isUpdate && (
+              <span className="quantity-label">{cartItem.quantity}</span>
+            )}
           </span>
-          <span className="update-quantity-link link-primary" onClick={openUpdate}>Update</span>
-          <span className="delete-quantity-link link-primary" onClick={deleteCartItem} >Delete</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={openUpdate}
+          >
+            Update
+          </span>
+          <span
+            className="delete-quantity-link link-primary"
+            onClick={deleteCartItem}
+          >
+            Delete
+          </span>
         </div>
       </div>
     </>
